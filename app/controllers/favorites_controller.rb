4b7aeee4b6ea163e321d7fd3368b16a89b59c536
tamam03 <tamam0308@gmail.com>
2022-04-11 11:37:04 +0000

    first commit

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,15 @@
+class FavoritesController < ApplicationController
+
+  def create
+    @book = Book.find(params[:book_id])
+    # favorite = current_user.favorites.new(book_id: book.id)
+    favorite = Favorite.new(book_id: @book.id, user_id: current_user.id)
+    favorite.save
+  end
+
+  def destroy
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
+    favorite.destroy
+  end
+end
---
+++ app/models/book_comment.rb
@@ -0,0 +1,9 @@
+class BookComment < ApplicationRecord
+  belongs_to :user
+  belongs_to :book
+  
+
+   has_one_attached :profile_image
+
+   validates :comment, presence: true
+end
